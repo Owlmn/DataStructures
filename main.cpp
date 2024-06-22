62ab26661ca776488aac0bcd12027187d7bba926
Owlmn <tregchannel@yandex.ru>
2024-06-22 21:12:01 +1000

    heap: call tests in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "heap/heap_tests.h"
 
+using namespace std;
+
+void testing(){
+    cout<<"Random heap: "; create_random_heap();
+    cout<<"Standard push: "; standard_push();
+    cout<<"Empty heap: "; create_empty_heap();
+    cout<<"Empty heap push: "; push_in_empty_heap();
+    cout<<"Top: "<<return_top()<<endl;
+    cout<<"Pop: "; pop_element_from_heap();
+    cout<<"Pop from empty: "; pop_from_empty();
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    testing();
     return 0;
 }
